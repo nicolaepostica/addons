954ed17231674b92f269c22741439b13782c9c0f
Postica Nicolae <nicolae.postica2@gmail.com>
2023-07-21 17:07:42 +0300

    drop down widget port to 16.0

--- drag_and_drop_binary_field/static/src/js/field_binary.js
+++ drag_and_drop_binary_field/static/src/js/field_binary.js
@@ -1,46 +1,52 @@
+/** @odoo-module **/
 // Copyright 2023 Nicolae Postica <nicolae.postica2@gmail.com>
-odoo.define('drag_and_drop_binary_field.drag_and_drop_widget', function (require) {
-    "use strict"
+import {registry} from "@web/core/registry"
+import {BinaryField} from "@web/views/fields/binary/binary_field"
+import {onMounted} from "@odoo/owl"
 
-    var fieldRegistry = require('web.field_registry')
-    var FieldBinaryFile = require('web.basic_fields').FieldBinaryFile
-
-    var FieldBinaryFileDragAndDrop = FieldBinaryFile.extend({
-        start: function () {
-            this._super.apply(this, arguments)
-            this.$el.on("drop", this.proxy("_onDropDown"))
-            this.$el.on("dragenter", this.proxy("_disableDefaultDragEvents"))
-            this.$el.on("dragover", this.proxy("_disableDefaultDragEvents"))
-            this.$el.on("dragleave", this.proxy("_disableDefaultDragEvents"))
-        },
-
-        _onDropDown: function (e) {
-            e.preventDefault()
-            e.stopPropagation()
-
-            const _$inputFile = this.$('.o_input_file')
-
-            if (_$inputFile.length === 0) return
-
-            const dataTransfer = e.originalEvent.dataTransfer
-
-            if (dataTransfer.files.length > 1) {
-                console.warn("Allowed only one file to upload")
-            }
-            else {
-                _$inputFile[0].files = dataTransfer.files
-                _$inputFile.trigger("change")
-            }
-        },
-
-        _disableDefaultDragEvents: function (e) {
-            e.preventDefault()
-        },
-    })
-
-    fieldRegistry.add('drag_and_drop', FieldBinaryFileDragAndDrop)
-
-    return {
-        FieldBinaryFileDragAndDrop: FieldBinaryFileDragAndDrop,
+export class FieldBinaryFileDragAndDrop extends BinaryField {
+    setup() {
+        super.setup()
+        onMounted(() => {
+            this.el = this.__owl__.bdom.parentEl
+            this.el.addEventListener("drop", (ev) => this._onDropDown(ev))
+            this.el.addEventListener("dragenter", this._disableDefaultDragEvents)
+            this.el.addEventListener("dragover", this._disableDefaultDragEvents)
+            this.el.addEventListener("dragleave", this._disableDefaultDragEvents)
+            this.input = this.el.querySelector('.o_input_file')
+        })
     }
-})
+
+    _onDropDown(ev) {
+        ev.preventDefault()
+
+        if (ev.dataTransfer.items) {
+            [...ev.dataTransfer.items].forEach((item, i) => {
+                if (item.kind === "file") {
+                    const file = item.getAsFile()
+                    this.uploadData(file)
+                }
+            })
+        }
+        else {
+            [...ev.dataTransfer.files].forEach((file, i) => {
+                this.uploadData(file)
+            })
+        }
+    }
+
+    uploadData(file) {
+        const customFileList = new DataTransfer()
+        customFileList.items.add(file)
+        this.input.files = customFileList.files
+        const event = new Event('change')
+        this.input.dispatchEvent(event)
+    }
+
+
+    _disableDefaultDragEvents(e) {
+        e.preventDefault()
+    }
+}
+
+registry.category('fields').add("drag_and_drop", FieldBinaryFileDragAndDrop)
